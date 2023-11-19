890e861d5075c1f410dae7ea7363aa15873daf58
b.wolska <beata.wolska@epikur.de>
2023-11-19 10:42:25 +0100

    a

---
+++ src/app/components/accordeon/accordeon.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccordeonComponent } from './accordeon.component';
+
+
+
+@NgModule({
+  declarations: [
+    AccordeonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AccordeonModule { }
