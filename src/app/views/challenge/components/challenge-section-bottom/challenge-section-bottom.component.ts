e64fa7a018224cf2f1e99ed08a274d4689c635fc
b.wolska <beata.wolska@epikur.de>
2023-11-26 11:58:38 +0100

    a

---
+++ src/app/views/challenge/components/challenge-section-bottom/challenge-section-bottom.component.ts
@@ -0,0 +1,16 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Word} from "@model";
+
+@Component({
+  selector: 'app-challenge-section-bottom',
+  templateUrl: './challenge-section-bottom.component.html',
+  styleUrls: ['./challenge-section-bottom.component.scss']
+})
+export class ChallengeSectionBottomComponent {
+  @Input() roundWords!: Word[];
+  @Output() action = new EventEmitter<any>();
+
+  handleWordAction(word_id: string): void {
+    this.action.emit(word_id);
+  }
+}
