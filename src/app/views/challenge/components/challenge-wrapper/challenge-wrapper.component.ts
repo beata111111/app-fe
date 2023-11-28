781432d17856d60eb0f0b7f54a289b5f5afe3b00
b.wolska <beata.wolska@epikur.de>
2023-11-28 17:10:52 +0100

    a

---
+++ src/app/views/challenge/components/challenge-wrapper/challenge-wrapper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge-wrapper',
+  templateUrl: './challenge-wrapper.component.html',
+  styleUrls: ['./challenge-wrapper.component.scss']
+})
+export class ChallengeWrapperComponent {
+
+}
